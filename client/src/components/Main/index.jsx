d9ef28bc7e01be8e204f09234e40b9f4f3bd89a2
gizemtpskl <75335500+gizemtpskl@users.noreply.github.com>
2022-10-30 21:14:47 +0300

    client side login-register

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,22 @@
+import styles from "./styles.modules.css";
+import React from "react";
+
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	return (
+		<div className={styles.main_container}>
+			<nav className={styles.navbar}>
+				<h1>fakebook</h1>
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Logout
+				</button>
+			</nav>
+		</div>
+	);
+};
+
+export default Main;
