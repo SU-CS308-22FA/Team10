6083206199d7ee83c0d4250760fc534f3a80963d
ebrarguler <ebrarsguler@gmail.com>
2022-11-23 14:21:04 +0300

    matches page is connected to database

--- frontend/src/components/Main/OvershadowedPlayer.jsx
+++ frontend/src/components/Main/OvershadowedPlayer.jsx
@@ -16,7 +16,7 @@ const OverShadowedPlayer = () => {
       async function fetchData() {
           console.log("Ebrar");
           const playerUrl =
-            "http://localhost:8080/api/player/637a8ed97757477ec0e7085b";
+            "http://localhost:8080/api/player/637a8d667757477ec0e70859";
             
           const fetchedPlayer = await axios.get(playerUrl);
           console.log(fetchedPlayer.data);
@@ -25,11 +25,27 @@ const OverShadowedPlayer = () => {
       fetchData();
 
     },[]);
-    
+
+    const [card2, setCard2] = React.useState({});
+  
+    React.useEffect(() => {
+      async function fetchData() {
+          console.log("Ebrar");
+          const playerUrl =
+            "http://localhost:8080/api/match/637dfd469953a84747abc2ba";
+            
+          const fetchedPlayer = await axios.get(playerUrl);
+          console.log(fetchedPlayer.data);
+          setCard2(fetchedPlayer.data);
+      } 
+      fetchData();
+
+    },[]);
     
       
         return(
-          <div className="row row-cols-15 row-cols-md-5 g-6">
+          
+          <div className="row row-cols-15 row-cols-md-4 g-6">
                 <div className="card-center cardalign w-50 h-50"  >
                 <h3 className="title">Highlighted Player of This Week</h3>
                     <div className="card h-100 w-100 text-center" >
@@ -55,31 +71,10 @@ const OverShadowedPlayer = () => {
                     </div>
                   
                 </div>
-                <div className="card-center cardalign w-50 h-50"  >
-                <h3 className="title">Upcoming Match: Very Soon!</h3>
-                    <div className="card h-100 w-100 text-center" >
-                    <div className="overflow">
-                      <img src={card.image} className="card-img-center" alt="..." object-fit = "fill"/>
-                    </div>
-                    <div className="card-body text-dark">
-                        <h5 className="card-title">{card.name}</h5>
-                        <p className="card-text">
-                          Team: {card.club} {<img src= {card.icon} width={23} height={23} alt="..."></img>} <br/> 
-                          Age: {card.age}<br/>
-                          Position: {card.position}<br/>
-                          Position Category: {card.position_category}<br/>
-                          Merket Vaue: {card.market_value}M <br/>
-                          Nationality: {card.nationality}
-                          </p>
-                        <Link to={card.link}>
-                            <button type="button" className="_btn">
-                               Go to profile
-                            </button>
-                         </Link>
-                    </div>
-                    </div>
+                
+                    
                   
-                </div>
+               
            
                 </div>
         );
@@ -92,6 +87,27 @@ export default OverShadowedPlayer;
 
 /*
 {
+  <div className="card h-100 w-100 text-center" >
+                    <div className="overflow">
+                      <img src={card.image} className="card-img-center" alt="..." object-fit = "fill"/>
+                    </div>
+                    <div className="card-body text-dark">
+                        <h5 className="card-title">{card.name}</h5>
+                        <p className="card-text">
+                          Team: {card.club} {<img src= {card.icon} width={23} height={23} alt="..."></img>} <br/> 
+                          Age: {card.age}<br/>
+                          Position: {card.position}<br/>
+                          Position Category: {card.position_category}<br/>
+                          Merket Value: {card.market_value} <br/>
+                          Nationality: {card.nationality}
+                          </p>
+                        <Link to={card.link}>
+                            <button type="button" className="_btn">
+                               Go to profile
+                            </button>
+                         </Link>
+                    </div>
+                    </div>
           image: img1,
           title: "Project 1",
           text: "Static Website",
@@ -147,5 +163,29 @@ export default OverShadowedPlayer;
           title: "Project 3",
           text: "Reactdskglkjklgjlkdfjgldkfgkldjfklgjdfklgjfklsdfnklsjgkjsdflkgjskdljfgklsjflkdsjkfjslkfjslkjflkdjsklfjdslk App",
           link:"#"
-        },
+        },,<div className="card-center cardalign w-50 h-50"  >
+                <h3 className="title">Upcoming Match: Very Soon!</h3>
+                <div className="card h w-100 text-center">
+                <div className="overflow">
+                {<img src= {card2.image1} width={70} alt="..."></img>} <br/> 
+                <p> </p>
+                <p> VS </p>
+                {<img src= {card2.image2} width={60} alt="..."></img>} <br/> 
+                </div>
+                <div class="card-body text-dark">
+                
+                  <p className="card-text">
+                        Team 1: {card2.team1} <br/> 
+                        Team 2: {card2.team2}<br/>
+                        Place: {card2.place}<br/>
+                        Date: {card2.date}<br/>
+                        Time: {card2.time} <br/>
+                        </p>
+                  <Link to={card2.link}>
+                    <button type="button" className="_btn">
+                      Go to match details
+                    </button>
+                  </Link>
+                </div>
+              </div>
 */
--- frontend/src/components/MatchesPage/MatchPage.jsx
+++ frontend/src/components/MatchesPage/MatchPage.jsx
@@ -1,7 +1,7 @@
 import styles from "./matchStyles.modules.css";
 import React from "react";
 
-import Cards from "./matchCards/matchCards";
+import CardsMatches from "./CardsMatch/CardsMatch";
 import ImageSlider from "./matchesSlides";
 import Header from "../Header/Header";
 const MatchesPage = () => {
@@ -13,15 +13,15 @@ const MatchesPage = () => {
 
       <p className="title">Last Week's Matches</p>
 
-      <Cards />
+      <CardsMatches />
 
       <p className="title">This Week's Matches</p>
 
-      <Cards />
+      <CardsMatches />
 
       <p className="title">Next Week's Matches</p>
 
-      <Cards />
+      <CardsMatches />
     </div>
   );
 };
