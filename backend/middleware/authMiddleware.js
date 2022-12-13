c89ef0a1bf9320af6c29641b77e93594ba5769d2
gizemtpskl <gizem-topsakal@hotmail.com>
2022-12-13 18:25:14 +0300

    added authMiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.status(403).send("Access denied.");
+
+    const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY);
+    req.user = decoded;
+    next();
+} catch (error) {
+    res.status(400).send("Invalid token");
+}
+
+  });
+
+
+  
+module.exports = { authMiddleware };
+
+
+
